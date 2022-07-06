38ab9df632be0834a18967d836d56917f6fe7336
Reinis Kalbergs <reinis.kalbergs@28stone.com>
2022-07-06 16:24:52 +0300

    Initial commit

---
+++ src/main/java/com/example/cameltask/CamelTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cameltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTaskApplication.class, args);
+	}
+
+}
